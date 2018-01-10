7ad4920c9415dd916f89bfc2acc3f8974190e948
Hunter Caron <huntercaron@mac.com>
2018-01-09 19:06:52 -0500

    starting to build out netlify cms structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,58 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+import Script from 'react-load-script';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const Container = new styled.div`
+
+`
+
+class IndexPage extends React.Component {
+  handleScriptLoad() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+    window.netlifyIdentity.init();
+  }
+
+  render() {
+    return(
+      <Container>
+
+        <h1>hey</h1>
+
+        <Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          onLoad={this.handleScriptLoad.bind(this)}/>
+
+      </Container>
+    )
+  }
+}
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage
