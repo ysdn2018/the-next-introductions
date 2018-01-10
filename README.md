58efb170173edf44844bff20c79cdc18252b4eb6
Hunter Caron <huntercaron@mac.com>
2018-01-10 15:04:45 -0500

    removing netlify cms for now

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Boilerplate base website for the next.
 
 Will be a basic setup of Gatsby with Styled-Components and Netlify CMS setup. It will be accompanied with a startup guide.
 
-*currently in progress*
+To get your own version, click the button below.
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/ysdn2018/the-next-base)
 
+*currently in progress*
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
-import Script from 'react-load-script';
 
 const Container = styled.div`
 
 `
 
 class IndexPage extends React.Component {
-  handleScriptLoad() {
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on('init', user => {
-        if (!user) {
-          window.netlifyIdentity.on('login', () => {
-            document.location.href = '/admin/';
-          });
-        }
-      });
-    }
-    window.netlifyIdentity.init();
-  }
-
   render() {
     return(
       <Container>
 
         <h1>hey</h1>
 
-        <Script
-          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          onLoad={this.handleScriptLoad.bind(this)}/>
+
 
       </Container>
     )
