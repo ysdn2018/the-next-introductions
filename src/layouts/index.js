6163be9232244ce860393ce1134ba0e64230cb63
Hunter Caron <huntercaron@mac.com>
2018-01-10 15:29:30 -0500

    adding fonts, cleaning up styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import styled, { injectGlobal } from 'styled-components'
 
-import './index.css'
+import RobotoMonoRegular from "../assets/fonts/RobotoMono-Regular.woff"
+import RobotoMonoRegular2 from "../assets/fonts/RobotoMono-Regular.woff2"
 
 const Header = () => (
   <div
@@ -35,26 +37,21 @@ const Header = () => (
 )
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <Container>
     <Helmet
-      title="Gatsby Default Starter"
+      title="the-next-base"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
+
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+
+    <div>
       {children()}
     </div>
-  </div>
+  </Container>
 )
 
 TemplateWrapper.propTypes = {
@@ -62,3 +59,39 @@ TemplateWrapper.propTypes = {
 }
 
 export default TemplateWrapper
+
+
+injectGlobal`
+  *, *:before, *:after {
+      box-sizing: border-box;
+      -webkit-overflow-scrolling: touch;
+  }
+
+  html {
+    font-size: 62.5%;
+    height: 100%;
+  }
+
+  body {
+      margin: 0;
+      height: 100%;
+      font-size: 1.6em;
+      line-height: 1.6;
+      font-weight: 400;
+      font-family: 'Roboto Mono', monospace;
+      color: #222;
+      webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+      text-rendering: optimizeLegibility;
+  }
+
+  @font-face {
+    font-family: 'Roboto Mono';
+    src: url(${RobotoMonoRegular}) format('woff2'),
+      url(${RobotoMonoRegular2}) format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+`
