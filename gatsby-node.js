17fe40b72649e35032036168947d4954170f634f
Hunter Caron <huntercaron@mac.com>
2018-01-31 13:30:05 -0500

    removing page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,34 +15,3 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
     fs.createReadStream("./src/content/" + node.relativePath).pipe(fs.createWriteStream(staticImagePath + node.base));
   }
 }
-
-exports.createPages = ({ boundActionCreators, graphql }) => {
- const { createPage } = boundActionCreators;
-
- return graphql(`
-   {
-     allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
-       edges {
-         node {
-           frontmatter {
-             templateKey
-             path
-           }
-         }
-       }
-     }
-   }
- `).then(result => {
-   if (result.errors) {
-     result.errors.forEach(e => console.error(e.toString()));
-     return Promise.reject(result.errors);
-   }
-   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-     createPage({
-       path: node.frontmatter.path,
-       component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
-       context: {} // additional data can be passed via context
-     });
-   });
- });
-};
