1544b322782c4fbf651a400fd30b041915d7594e
Hunter Caron <huntercaron@mac.com>
2018-02-13 12:18:22 -0500

    modify webpack config for intersection-observer

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,50 +16,13 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   }
 }
 
-// exports.modifyWebpackConfig = function({config, env}) {
-//   config.merge({
-//     resolve: {
-//       alias: {
-//           "TweenLite": Path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
-//           "TweenMax": Path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
-//           "TimelineLite": Path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
-//           "TimelineMax": Path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
-//           "ScrollMagic": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
-//           "animation.gsap": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
-//           "debug.addIndicators": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
-//       }
-//     }
-//   });
-//   return config;
-// }
 
-// exports.createPages = ({ boundActionCreators, graphql }) => {
-//  const { createPage } = boundActionCreators;
-//
-//  return graphql(`
-//    {
-//      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
-//        edges {
-//          node {
-//            frontmatter {
-//              templateKey
-//              path
-//            }
-//          }
-//        }
-//      }
-//    }
-//  `).then(result => {
-//    if (result.errors) {
-//      result.errors.forEach(e => console.error(e.toString()));
-//      return Promise.reject(result.errors);
-//    }
-//    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//      createPage({
-//        path: node.frontmatter.path,
-//        component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
-//        context: {} // additional data can be passed via context
-//      });
-//    });
-//  });
-// };
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  if (stage === "build-html") {
+    config.loader("null", {
+      test: /intersection-observer/,
+      loader: "null-loader",
+    });
+  }
+};
