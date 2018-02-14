2ad6f80355388e227f9471f2da8fea6dbe0bede2
Hunter Caron <huntercaron@mac.com>
2018-02-14 17:37:08 -0500

    new students and padding tweak

--- src/components/BottomNav/index.js
+++ src/components/BottomNav/index.js
@@ -172,12 +172,12 @@ const EventInfo = GridBlock.extend`
     font-size: 1.5rem;
     left: calc(${calcColumns(1)}% - 2px);
     top: calc(${calcRows(2)}% + 0px);
-    height: calc(${calcRows(2)}% - 34px);
+    height: calc(${calcRows(2)}% - 30px);
     width: calc(64% + 3px);
   }
 
   @media (max-width: 450px) {
-    height: calc(${calcRows(2)}% - 22px);
+    height: calc(${calcRows(2)}% - 18px);
   }
 
   @media (min-width: 500px) and (max-width: ${breakpoints.mobile}) {
