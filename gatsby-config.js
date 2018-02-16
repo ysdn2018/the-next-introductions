1dfcdb279560459ddde8901f3f135972868a3dfd
Hunter Caron <huntercaron@mac.com>
2018-02-16 11:35:15 -0500

    react 16

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     // `gatsby-plugin-preact`,
+    `gatsby-plugin-react-next`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
